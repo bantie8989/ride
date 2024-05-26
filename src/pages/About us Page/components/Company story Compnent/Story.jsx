34aafd00c18dde1dff0ef1ffed0fe0cdf619e9fc
bantie8989 <146993130+bantie8989@users.noreply.github.com>
2024-05-26 15:58:06 +0300

    do the styling of story component

--- src/pages/About us Page/components/Company story Compnent/Story.jsx
+++ src/pages/About us Page/components/Company story Compnent/Story.jsx
@@ -1,18 +1,22 @@
 
 import React from 'react';
 import { Box, Text, Heading } from '@chakra-ui/react';
+import { FaHistory } from 'react-icons/fa';
 
 const story = () => {
   return (
-    <Box mt={8}>
-      <Heading size="lg" mb={4}>
+    <Box m={100}>
+      <Heading size="lg" mb={4} textAlign={'center'} fontFamily={'initial'}>
         Our Story
       </Heading>
-      <Text fontSize="md">
+      <Box display={'flex'} alignItems={'center'}>
+      <FaHistory size={500}/>
+      <Text fontSize="md" fontWeight={'bolder'} ml={10}>
         Our ride-sharing program was born out of a desire to make transportation more accessible and convenient for everyone. We were inspired by the idea of creating a community where people could share rides and reduce their carbon footprint.
 
         Since our launch, we've helped thousands of people get from point A to point B, and we're proud to be a part of the movement towards sustainable transportation.
       </Text>
+      </Box>
     </Box>
   );
 };
