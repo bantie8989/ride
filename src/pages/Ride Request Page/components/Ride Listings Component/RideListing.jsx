298b21c39f8cb73a3fdcd87737c3f326fced08bb
bantie8989 <146993130+bantie8989@users.noreply.github.com>
2024-05-26 17:30:19 +0300

    fix the the car using grid element

--- src/pages/Ride Request Page/components/Ride Listings Component/RideListing.jsx
+++ src/pages/Ride Request Page/components/Ride Listings Component/RideListing.jsx
@@ -14,6 +14,8 @@ import {
   Avatar,
   Badge,
   grid,
+  SimpleGrid,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { useState } from "react";
 
@@ -74,6 +76,150 @@ const RideListing = () => {
       carType: "Fuel-efficient",
       driverPrefs: ["Non-smoking", "No music"],
     },
+    {
+      id: 4,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
+    {
+      id: 5,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
+    {
+      id: 6,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
+    {
+      id: 7,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
+    {
+      id: 8,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
+    {
+      id: 9,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
+    {
+      id: 10,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
+    {
+      id: 11,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
+    {
+      id: 12,
+      driver: {
+        name: "John Doe",
+        profilePicture: "https://example.com/johndoe.jpg",
+        rating: 4.5,
+      },
+      origin: "New York",
+      destination: "Los Angeles",
+      travelTime: "4 hours",
+      cost: " $50.00",
+      numSeats: 4,
+      vehicle: "Toyota Camry",
+      carType: "Fuel-efficient",
+      driverPrefs: ["Non-smoking", "No music"],
+    },
     // ...
   ]);
 
@@ -93,8 +239,10 @@ const RideListing = () => {
     return true;
   });
 
+  const columnCount = useBreakpointValue({ base: 1, md: 2, lg: 3 });
+
   return (
-    <Box maxW="md" mx="auto" p={4}>
+    <Box w={"100%"} p={10} m={10}>
       {/* Filter section */}
       <VStack mb={4}>
         <Text fontSize="lg" mb={2}>
@@ -134,60 +282,73 @@ const RideListing = () => {
       </VStack>
 
       {/* Ride listings section */}
-      <Grid templateColumns="repeat(1, 1fr)" gap={14}>
-        {filteredRideListings.map((ride) => (
-          <GridItem key={ride.id}>
-            <Flex
-              direction="row"
-              alignItems="center"
-              justifyContent="space-between"
-              mb={2}
-            >
-              <Avatar src={ride.driver.profilePicture} size="sm" />
-              <Text fontSize="lg" fontWeight="bold" ml={2}>
-                {ride.driver.name}
-              </Text>
-              <Badge variant="outline" colorScheme="green" fontSize="sm" ml={2}>
-                {ride.driver.rating} stars
-              </Badge>
-            </Flex>
-            <Text fontSize="md" mb={2}>
-              {ride.origin} → {ride.destination}
-            </Text>
-            <Text fontSize="md" mb={2}>
-              Estimated travel time: {ride.travelTime}
-            </Text>
-            <Text fontSize="md" mb={2}>
-              Estimated cost: {ride.cost}
-            </Text>
-            <Text fontSize="md" mb={2}>
-              Number of seats available: {ride.numSeats}
-            </Text>
-            {ride.vehicle && (
-              <Text fontSize="md" mb={2}>
-                Vehicle: {ride.vehicle}
-              </Text>
-            )}
-            <Text fontSize="md" mb={2}>
-              Driver preferences:{" "}
-              {ride.driverPrefs.map((pref, index) => (
-                <Badge key={index} variant="outline" colorScheme="gray" mr={1}>
-                  {pref}
+
+      <SimpleGrid columns={columnCount} spacing={18} mt={10}>
+        {filteredRideListings.map((ride , index) => (
+          <Grid templateColumns="repeat(1, 1fr)" gap={14} key={index}>
+            <GridItem key={ride.id} p={10}>
+              <Flex
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                mb={2}
+              >
+                <Avatar src={ride.driver.profilePicture} size="sm" />
+                <Text fontSize="lg" fontWeight="bold" ml={2}>
+                  {ride.driver.name}
+                </Text>
+                <Badge
+                  variant="outline"
+                  colorScheme="green"
+                  fontSize="sm"
+                  ml={2}
+                >
+                  {ride.driver.rating} stars
                 </Badge>
-              ))}
-            </Text>
-            <Button
-              colorScheme="blue"
-              variant="solid"
-              size="sm"
-              w="100%"
-              mb={4}
-            >
-              Select
-            </Button>
-          </GridItem>
+              </Flex>
+              <Text fontSize="md" mb={2}>
+                {ride.origin} → {ride.destination}
+              </Text>
+              <Text fontSize="md" mb={2}>
+                Estimated travel time: {ride.travelTime}
+              </Text>
+              <Text fontSize="md" mb={2}>
+                Estimated cost: {ride.cost}
+              </Text>
+              <Text fontSize="md" mb={2}>
+                Number of seats available: {ride.numSeats}
+              </Text>
+              {ride.vehicle && (
+                <Text fontSize="md" mb={2}>
+                  Vehicle: {ride.vehicle}
+                </Text>
+              )}
+              <Text fontSize="md" mb={2}>
+                Driver preferences:{" "}
+                {ride.driverPrefs.map((pref, index) => (
+                  <Badge
+                    key={index}
+                    variant="outline"
+                    colorScheme="gray"
+                    mr={1}
+                  >
+                    {pref}
+                  </Badge>
+                ))}
+              </Text>
+              <Button
+                colorScheme="blue"
+                variant="solid"
+                size="sm"
+                w="100%"
+                mb={4}
+              >
+                Select
+              </Button>
+            </GridItem>
+          </Grid>
         ))}
-      </Grid>
+      </SimpleGrid>
     </Box>
   );
 };
