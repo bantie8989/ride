4112560fb20e794d1304b8232ecd30061510c168
bantie8989 <146993130+bantie8989@users.noreply.github.com>
2024-05-26 23:52:49 +0300

    fixt profile css

--- src/pages/Account Page/components/Profile Information Componet/Profile.jsx
+++ src/pages/Account Page/components/Profile Information Componet/Profile.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import { Box, Text, Heading, VStack, Avatar, Center } from "@chakra-ui/react";
+import { FaMailBulk, FaPhone, FaVoicemail } from "react-icons/fa";
 
 const Profile = () => {
   return (
     <Box>
       <Center>
         <VStack>
-          <Heading size="lg" mb={4}>
+          <Heading size="lg" mb={29} mt={10}>
             Profile Information
           </Heading>
-          <VStack spacing={4} alignItems="center">
+          <VStack
+            spacing={4}
+            alignItems="center"
+            mt={20}
+            mb={20}
+            fontWeight={"bolder"}
+            fontFamily={"initial"}
+            color={"goldenrod"}
+          >
             <Avatar size="xl" src="https://bit.ly/sage-adebayo" />
-            <Text fontSize="md" fontWeight="bold">
+            <Text fontSize="md" fontWeight="bold" mb={10}>
               John Doe
             </Text>
-            <Text fontSize="md">johndoe@example.com</Text>
-            <Text fontSize="md">+1 (123) 456-7890</Text>
+            <Box display={"flex"} alignItems={"center"}>
+              <FaMailBulk size={30} />
+              <Text ml={5} fontSize="md">
+                johndoe@example.com
+              </Text>
+            </Box>
+            <Box display={"flex"} alignItems={"center"}>
+              <FaPhone size={30} />
+
+              <Text ml={30} fontSize="md">
+                +1 (123) 456-7890
+              </Text>
+            </Box>
           </VStack>
         </VStack>
       </Center>
